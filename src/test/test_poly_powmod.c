0ad7c195dc275e92a30419d4e09bc4d7dccfc3b4
Gabriel Eiseman <hacatu5000@gmail.com>
2023-04-02 05:02:16 -0400

    Did you know that github actions use dash instead of bash by default?
    I will be posting about this on a meme subreddit later

---
+++ src/test/test_poly_powmod.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include <nut/modular_math.h>
+#include <nut/polynomial.h>
+
+const int64_t M = 20092010ull;
+const int64_t K = 1000000000000000000ull;
+const int64_t N = 2000ull;
+
+int main(){
+	poly_t f[1], a[1], x[1];
+	if(!init_poly(f, N + 1) || !init_poly(a, N) || !init_poly(x, 2)){
+		fprintf(stderr, "\e[1;31mERROR: Could not allocate polynomials.\e[0m\n");
+		exit(EXIT_FAILURE);
+	}
+	memset(f->coeffs + 2, 0, (N - 1 - 2 + 1)*sizeof(int64_t));
+	f->coeffs[0] = M - 1;
+	f->coeffs[1] = M - 1;
+	f->coeffs[N] = 1;
+	f->len = N + 1;
+	
+	x->coeffs[0] = 0;
+	x->coeffs[1] = 1;
+	x->len = 2;
+	
+	powmod_poly_modn_tmptmp(a, x, K - N + 1, f, M);
+	
+	uint64_t s = a->coeffs[0];
+	for(uint64_t j = 1; j < (uint64_t)N; ++j){
+		s = mod(s + a->coeffs[j]*2, M);
+	}
+	
+	printf("%"PRIu64"\n", s);
+	
+	destroy_poly(f);
+	destroy_poly(x);
+	destroy_poly(a);
+}
+
