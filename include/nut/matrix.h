23a28e4c079832ab5521095fc4ac6e2de307a494
Gabriel Eiseman <hacatu5000@gmail.com>
2025-01-03 15:38:44 -0500

    Added u64 modular matrices

--- include/nut/matrix.h
+++ include/nut/matrix.h
@@ -19,6 +19,11 @@ typedef struct{
 	int64_t *buf;
 } nut_i64_Matrix;
 
+typedef struct{
+	uint64_t rows, cols, modulus;
+	uint64_t *buf;
+} nut_u64_ModMatrix;
+
 /// Allocate internal buffers for a matrix
 /// @param [in] rows, cols: The number of rows and columns.  Must not be negative.
 /// If either is zero, most operations will become no-ops.
@@ -41,7 +46,7 @@ NUT_ATTR_ACCESS(read_write, 1)
 void nut_i64_Matrix_destroy(nut_i64_Matrix *self);
 
 /// Print a matrix to a given file.
-/// Format is '[[0, 1, 2],\n[3, 4, 5],\n[6, 7, 8]]
+/// Format is '[[0, 1, 2],\n[3, 4, 5],\n[6, 7, 8]]'
 NUT_ATTR_NONNULL(1, 2)
 NUT_ATTR_ACCESS(read_only, 1)
 NUT_ATTR_ACCESS(read_write, 2)
@@ -82,12 +87,12 @@ void nut_i64_Matrix_fill_short_pascal(nut_i64_Matrix *self);
 /// the result can be interpreted as a rational matrix.  This denominator may not be reduced in general.
 /// Uses gaussian elimination specialized to lower triangular matrices.
 /// @param [in] self: The matrix to invert, which is clobbered in place.  If the original matrix will still be needed, copy it beforehand
-/// @param [out] tmp: The matrix to store the result in
+/// @param [out] out: The matrix to store the result in
 /// @return 0 if self/tmp have different sizes or are not square or self is not invertable, otherwise the denominator (all entries should be understood as numeratos)
 NUT_ATTR_NONNULL(1, 2)
 NUT_ATTR_ACCESS(read_write, 1)
 NUT_ATTR_ACCESS(read_write, 2)
-int64_t nut_i64_Matrix_invert_ltr(nut_i64_Matrix *restrict self, nut_i64_Matrix *restrict tmp);
+int64_t nut_i64_Matrix_invert_ltr(nut_i64_Matrix *restrict self, nut_i64_Matrix *restrict out);
 
 /// Fill a vector with increasing powers of x
 /// @param [in] x: The number to take powers of
@@ -95,4 +100,82 @@ int64_t nut_i64_Matrix_invert_ltr(nut_i64_Matrix *restrict self, nut_i64_Matrix 
 /// @param [in] m: The modulus to reduce powers by, or 0 to skip reducing
 /// @param [out] out: Buffer to store resulting vector in.  The first entry will be set to x%m, then x*x%m then x*x*x%m, etc
 void nut_i64_Matrix_fill_vandemond_vec(uint64_t x, uint64_t k, uint64_t m, int64_t out[static k + 1]);
+
+/// Allocate internal buffers for a matrix
+/// @param [in] rows, cols: The number of rows and columns.
+/// If either is zero, most operations will become no-ops.
+/// Many operations are specific to square matrices
+/// @param [in] modulus: The modulus.  Some operations may fail if it is not prime.
+/// @param [out] self: matrix to initialize
+/// @return true on success, false on failure (allocation failure, size overflow, negative size)
+NUT_ATTR_NONNULL(1)
+NUT_ATTR_ACCESS(write_only, 1)
+bool nut_u64_ModMatrix_init(nut_u64_ModMatrix *self, uint64_t rows, uint64_t cols, uint64_t modulus);
+
+/// Copy the values from one matrix to another, which must be initialized
+NUT_ATTR_NONNULL(1, 2)
+NUT_ATTR_ACCESS(read_write, 1)
+NUT_ATTR_ACCESS(read_only, 2)
+void nut_u64_ModMatrix_copy(nut_u64_ModMatrix *restrict dest, const nut_u64_ModMatrix *restrict src);
+
+/// Deallocate internal buffers for a matrix table
+NUT_ATTR_NONNULL(1)
+NUT_ATTR_ACCESS(read_write, 1)
+void nut_u64_ModMatrix_destroy(nut_u64_ModMatrix *self);
+
+/// Print a matrix to a given file.
+/// Format is '[[0, 1, 2],\n[3, 4, 5],\n[6, 7, 8]] mod 1000000009'
+NUT_ATTR_NONNULL(1, 2)
+NUT_ATTR_ACCESS(read_only, 1)
+NUT_ATTR_ACCESS(read_write, 2)
+void nut_u64_ModMatrix_fprint(const nut_u64_ModMatrix *restrict self, FILE *file);
+
+/// Fill a matrix with zeros off the diagonal and ones on the diagonal
+/// The matrix must be squared
+NUT_ATTR_NONNULL(1)
+NUT_ATTR_ACCESS(read_write, 1)
+bool nut_u64_ModMatrix_fill_I(nut_u64_ModMatrix *self);
+
+/// Multiply a matrix by a vector, returning a vector
+/// The vectors are stored as one dimensional arrays
+/// This is a special case of matrix-matrix multiplication, since a row/column
+/// vec is simply a matrix with 1 row/column respectively.
+/// The advantage is that this can interprest more things (any buffer) as a vector.
+/// @param [in] self: The matrix to multiply the input vector by
+/// @param [in] vec: The vector to multiply.  The number of cols in the matrix must match the length of this vector.
+/// If at least self->cols elements of vec are not accessible, it is undefined behavior.
+/// @param [out] out: The vector to store the result in.  The number of rows in the matrix must match the length of this vector.
+/// If at least self->rows elements of out are not accessible, it is undefined behavior
+NUT_ATTR_NONNULL(1, 2, 3)
+NUT_ATTR_ACCESS(read_only, 1)
+NUT_ATTR_ACCESS(read_only, 2)
+NUT_ATTR_ACCESS(write_only, 3)
+void nut_u64_ModMatrix_mul_vec(const nut_u64_ModMatrix *restrict self, const uint64_t vec[restrict static self->cols], uint64_t out[restrict static self->rows]);
+
+/// Fill a matrix with part of the Pascal Triangle
+/// This is mostly internally used together with { @link nut_u64_ModMatrix_invert_ltr } to get a matrix of Faulhaber polynomial
+/// coefficients to quickly sum powers of integer (see { @link nut_Diri_compute_Nk }).
+/// In particular, the ith row of the matrix will be filled by the ith row of pascal's triangle without the final 1,
+/// trimmed/zero padded as needed.  The first (0th) row is a 1 followed by all zeroes, the second (1st) row is a 1,
+/// then a 2, then all zeroes, etc.
+void nut_u64_ModMatrix_fill_short_pascal(nut_u64_ModMatrix *self);
+
+/// Invert a lower triangular matrix
+/// Since we are working on matrices mod some number, any integer matrix whose denominator is divisible by the modulus, even if it is not zero,
+/// will be non-invertable.
+/// Uses gaussian elimination specialized to lower triangular matrices.
+/// @param [in] self: The matrix to invert, which is clobbered in place.  If the original matrix will still be needed, copy it beforehand
+/// @param [out] out: The matrix to store the result in
+/// @return false if self/tmp have different sizes or are not square or self is not invertable, otherwise true
+NUT_ATTR_NONNULL(1, 2)
+NUT_ATTR_ACCESS(read_write, 1)
+NUT_ATTR_ACCESS(read_write, 2)
+bool nut_u64_ModMatrix_invert_ltr(nut_u64_ModMatrix *restrict self, nut_u64_ModMatrix *restrict out);
+
+/// Fill a vector with increasing powers of x
+/// @param [in] x: The number to take powers of
+/// @param [in] k: The max exponent (inclusive)
+/// @param [in] m: The modulus to reduce powers by, may not be zero
+/// @param [out] out: Buffer to store resulting vector in.  The first entry will be set to x%m, then x*x%m then x*x*x%m, etc
+void nut_u64_Matrix_fill_vandemond_vec(uint64_t x, uint64_t k, uint64_t m, uint64_t out[static k + 1]);
 
--- src/lib/nut/matrix.c
+++ src/lib/nut/matrix.c
@@ -149,3 +149,139 @@ void nut_i64_Matrix_fill_vandemond_vec(uint64_t x, uint64_t k, uint64_t m, int64
 	}
 }
 
+bool nut_u64_ModMatrix_init(nut_u64_ModMatrix *self, uint64_t rows, uint64_t cols, uint64_t modulus){
+	size_t nbytes;
+	if(__builtin_mul_overflow(rows, cols, &nbytes) || __builtin_mul_overflow(nbytes, sizeof(uint64_t), &nbytes)){
+		return false;
+	}
+	if(!(self->buf = malloc(nbytes))){
+		return false;
+	}
+	self->rows = rows;
+	self->cols = cols;
+	self->modulus = modulus;
+	return true;
+}
+
+void nut_u64_ModMatrix_copy(nut_u64_ModMatrix *restrict dest, const nut_u64_ModMatrix *restrict src){
+	memcpy(dest->buf, src->buf, src->rows*src->cols*sizeof(uint64_t));
+}
+
+void nut_u64_ModMatrix_destroy(nut_u64_ModMatrix *self){
+	free(self->buf);
+	self->buf = NULL;
+}
+
+void nut_u64_ModMatrix_fprint(const nut_u64_ModMatrix *restrict self, FILE *file){
+	fprintf(file, "[");
+	for(uint64_t i = 0; i < self->rows; ++i){
+		fprintf(file, "[");
+		for(uint64_t j = 0; j < self->cols; ++j){
+			uint64_t a = self->buf[self->cols*i + j];
+			if(j + 1 == self->cols){
+				fprintf(file, "%"PRIu64, a);
+			}else{
+				fprintf(file, "%"PRIu64", ", a);
+			}
+		}
+		fprintf(file, "]");
+		if(i + 1 < self->cols){
+			fprintf(file, ",\n");
+		}
+	}
+	fprintf(file, "] mod %"PRIu64"", self->modulus);
+}
+
+bool nut_u64_ModMatrix_fill_I(nut_u64_ModMatrix *self){
+	if(self->rows != self->cols){
+		return false;
+	}
+	if(!self->rows){
+		return true;
+	}
+	memset(self->buf, 0, self->rows*self->cols*sizeof(uint64_t));
+	for(uint64_t i = 0; i < self->rows; ++i){
+		self->buf[self->rows*i + i] = 1;
+	}
+	return true;
+}
+
+void nut_u64_ModMatrix_mul_vec(const nut_u64_ModMatrix *restrict self, const uint64_t vec[restrict static self->cols], uint64_t out[restrict static self->rows]){
+	if(self->rows){
+		memset(out, 0, self->rows*sizeof(uint64_t));
+	}
+	for(uint64_t row = 0; row < self->rows; ++row){
+		for(uint64_t col = 0; col < self->cols; ++col){
+			out[row] = (out[row] + self->buf[self->cols*row + col]*vec[col])%self->modulus;
+		}
+	}
+}
+
+void nut_u64_ModMatrix_fill_short_pascal(nut_u64_ModMatrix *self){
+	if(self->rows*self->cols == 0){
+		return;
+	}
+	for(uint64_t row = 0; row < self->rows; ++row){
+		self->buf[self->cols*row] = 1;
+		if(row){
+			self->buf[self->cols*row + 1] = (row + 1)%self->modulus;
+		}
+		for(uint64_t col = 2; col < row && col < self->cols; ++col){
+			uint64_t v = self->buf[self->cols*(row - 1) + col - 1] + self->buf[self->cols*(row - 1) + col];
+			self->buf[self->cols*row + col] = v >= self->modulus ? v - self->modulus : v;
+		}
+		if(row < self->cols){
+			self->buf[self->cols*row + row] = (row + 1)%self->modulus;
+		}
+		if(row + 1 < self->cols){
+			memset(self->buf + self->cols*row + row + 1, 0, (self->cols - row - 1)*sizeof(uint64_t));
+		}
+	}
+}
+
+bool nut_u64_ModMatrix_scale_row(nut_u64_ModMatrix *self, uint64_t row, uint64_t col_start, uint64_t a){
+	for(uint64_t i = col_start; i < self->cols; ++i){
+		self->buf[self->cols*row + i] *= a;
+		self->buf[self->cols*row + i] %= self->modulus;
+	}
+	return true;
+}
+
+bool nut_u64_ModMatrix_addmul_row(nut_u64_ModMatrix *self, uint64_t i, uint64_t j, uint64_t a){
+	for(uint64_t k = 0; k < self->cols; ++k){
+		self->buf[self->cols*j + k] += a*self->buf[self->cols*i + k];
+		self->buf[self->cols*j + k] %= self->modulus;
+	}
+	return true;
+}
+
+bool nut_u64_ModMatrix_invert_ltr(nut_u64_ModMatrix *restrict self, nut_u64_ModMatrix *restrict out){
+	if(self->rows != self->cols || self->rows != out->rows || self->cols != out->rows){
+		return 0;
+	}
+	for(uint64_t i = 0; i < self->rows; ++i){
+		if(!self->buf[self->cols*i + i]){
+			return false;
+		}
+	}
+	nut_u64_ModMatrix_fill_I(out);
+	for(uint64_t i = 0; i < self->rows; ++i){
+		uint64_t a = self->buf[self->cols*i + i];
+		int64_t _ainv = nut_i64_modinv(a, self->modulus);
+		nut_u64_ModMatrix_scale_row(out, i, 0, (uint64_t)_ainv);
+		for(uint64_t j = i + 1; j < self->rows; ++j){
+			uint64_t b = self->buf[self->cols*j + i];
+			if(b){
+				nut_u64_ModMatrix_addmul_row(out, i, j, self->modulus - b);
+			}
+		}
+	}
+	return true;
+}
+
+void nut_u64_Matrix_fill_vandemond_vec(uint64_t x, uint64_t k, uint64_t m, uint64_t out[static k + 1]){
+	for(uint64_t e = 1, xe = x; e <= k; ++e, xe = xe*x%m){
+		out[e - 1] = xe;
+	}
+}
+
